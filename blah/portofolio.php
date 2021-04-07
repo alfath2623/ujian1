41dc3337d1282cf082cd91f155805da7a7d59b5e
alfath2623 <64334976+alfath2623@users.noreply.github.com>
2021-04-07 15:12:56 +0700

    Add files via upload

---
+++ blah/portofolio.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instadocs</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/7e65c369c0.js" crossorigin="anonymous"></script>
+    <script src="stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="wrapper">
+<div class="sidebar">
+       <h2>Sidebar</h2>
+       <ul>
+           <li><a href="index.php"><i class="fas fa-home"></i>home</a></li>
+           <li><a href="profil.php"><i class="fas fa-user"></i>Profil</a></li>
+           <li><a href="about.php"><i class="fas fa-address-card"></i>About</a></li>
+           <li><a href="portofolio.php"><i class="fas fa-project-diagram"></i>Portofolio</a></li>
+           <li><a href="blog.php"><i class="fas fa-blog"></i>Blogs</a></li>
+           <li><a href="phone.php"><i class="fas fa-address-book"></i>phone</a></li>
+           <li><a href="login.php"><i class="fas fa-map-pin"></i></a></li>           
+       </ul>
+
+       <div class="social_media">
+           <a href="#"><i class="fab fa-facebook"></i></a>
+           <a href="#"><i class="fab fa-instagram"></i></a>
+           <a href="#"><i class="fab fa-twitter"></i></a>
+       </div>
+   </div>
+   <div class="main_contect">
+       <div class="header">Welcome!! have a nice day</div>
+       <section id="box-profile">
+       <div class="info">
+           <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos adipisci minima sit officia, quod dolor fuga maxime omnis vitae amet esse enim voluptates nesciunt eius nisi ad incidunt reprehenderit doloremque! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur ipsum ea odio architecto similique quidem delectus nemo assumenda quam! Possimus adipisci, perferendis rem fugiat veniam sunt temporibus provident nobis molestias.</div>
+           <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt ad odit consequatur, impedit, sequi quae adipisci suscipit culpa voluptatibus fugit provident obcaecati error at sint asperiores voluptates mollitia ut recusandae. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis voluptatibus laboriosam unde ea hic repellat voluptate non optio, soluta deleniti recusandae at facilis, exercitationem amet natus. Aliquid adipisci quod dolor.</div>
+           <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta minus quaerat laboriosam magni mollitia, ratione atque in, fuga sed, exercitationem eaque dolorem impedit? Voluptatum, natus nobis voluptas nam repellendus eligendi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam illo minima, totam deleniti vitae voluptatum placeat aliquid debitis molestias quis tenetur, dolorum voluptate aperiam quo iste. Ea nisi sed minima!</div>
+       </div>
+       </section>
+       
+   </div>
+</div>
+   
+</body>
+</html>
